face2452ee8e1b3a6682866d199458bf9b43a84e
Justin Yocus <justin.o.yocus@gmail.com>
2020-11-30 22:30:15 -0600

    questions and answers displaying (with a duplication issue) but able to loop through question array

--- script.js
+++ script.js
@@ -1,23 +1,71 @@
 const startButton = document.getElementById('start-btn')
 const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
 
 function startGame() {
     console.log('Started');
     startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() -  .5)
+    currentQuestionIndex = 0
     questionContainerElement.classList.remove('hide')
+    setNextQuestion()
 }
 
 function setNextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
 
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct 
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+    });
 }
 
 function selectAnswer() {
 
 }
 
+//Questions Array
+const questions = [ {
+    question:"What is 1+1",
+    answers: [
+        { text: "2", correct: true },
+        { text: "4", correct: false },
+        { text: "6", correct: false },
+        { text: "8", correct: false }
+    ],
+},{
+    question:"What is 2+2",
+    answers: [
+        { text: "2", correct: true },
+        { text: "4", correct: false },
+        { text: "6", correct: false },
+        { text: "8", correct: false }
+    ],
+},{
+    question:"What is 4+4",
+    answers: [
+        { text: "2", correct: true },
+        { text: "4", correct: false },
+        { text: "6", correct: false },
+        { text: "8", correct: false }
+    ]
+}
 
+]
 
 
 
@@ -71,7 +119,7 @@ function selectAnswer() {
 // //     }, 1000);
 // // }
 
-// //Questions Array
+//Questions Array
 // var questions = [ {
 //     question:"What is 1+1",
 //     choices: [
